7d09f662f0e1390c0261dea8fe52cb8072b6a73a
Damian Arp <damianarp@gmail.com>
2020-11-10 22:43:49 -0300

    Pagina Proyectos

--- js/lightbox.js
+++ js/lightbox.js
@@ -41,7 +41,7 @@
   // Descriptions of all options available on the demo site:
   // http://lokeshdhakar.com/projects/lightbox2/index.html#options
   Lightbox.defaults = {
-    albumLabel: 'Image %1 of %2',
+    albumLabel: 'Imagen %1 de %2',
     alwaysShowNavOnTouchDevices: false,
     fadeDuration: 600,
     fitImagesInViewport: true,
