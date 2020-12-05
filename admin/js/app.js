e6db77cfbd3598612c616676ebdc81dd6384530e
Damian Arp <damianarp@gmail.com>
2020-12-04 22:12:40 -0300

    cambio de posicion de funciones en app.js

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
 
+    // Funciones de los formularios de creación y edición de Admins
+    $('#crear-registro').attr('disabled', true);
+    $('#modificar_registro').attr('disabled', true);
+
+    $('#repetir_password').on('input', function() {
+        var password_nuevo = $('#password').val();
+
+        if($(this).val() == password_nuevo) {
+            $('#resultado_password').text('Correcto');
+            $('#resultado_password').parents('.form-group').addClass('has-success').removeClass('has-error');
+            $('input#password').parents('.form-group').addClass('has-success').removeClass('has-error');
+            $('#crear-registro').attr('disabled', false);
+            $('#modificar_registro').attr('disabled', false);
+        } else {
+            $('#resultado_password').text('No son iguales');
+            $('#resultado_password').parents('.form-group').addClass('has-error').removeClass('has-success');
+            $('input#password').parents('.form-group').addClass('has-error').removeClass('has-success');
+        }
+    });
+
     // Paginación de AdminLTE --> Marca error en consola y no funciona
     $('.sidebar-menu').tree();
 
@@ -25,25 +45,7 @@ $(document).ready(function () {
         }
     });
 
-    // Funciones de los formularios de creación y edición de Admins
-    $('#crear-registro').attr('disabled', true);
-    $('#modificar_registro').attr('disabled', true);
-
-    $('#repetir_password').on('input', function() {
-        var password_nuevo = $('#password').val();
-
-        if($(this).val() == password_nuevo) {
-            $('#resultado_password').text('Correcto');
-            $('#resultado_password').parents('.form-group').addClass('has-success').removeClass('has-error');
-            $('input#password').parents('.form-group').addClass('has-success').removeClass('has-error');
-            $('#crear-registro').attr('disabled', false);
-            $('#modificar_registro').attr('disabled', false);
-        } else {
-            $('#resultado_password').text('No son iguales');
-            $('#resultado_password').parents('.form-group').addClass('has-error').removeClass('has-success');
-            $('input#password').parents('.form-group').addClass('has-error').removeClass('has-success');
-        }
-    });
+    
 });
 
   
