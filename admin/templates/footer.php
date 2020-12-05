11bf945195a27b65996a66e02382888920630a13
Damian Arp <damianarp@gmail.com>
2020-12-04 21:17:29 -0300

    se cambio la ruta del archivo app.js en el header (ahora funciona)

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -47,7 +47,7 @@
         <script src="js/posteo.js"></script>  
         <script src="js/login-ajax.js"></script>
         <script src="js/admin-ajax.js"></script>
-        <script src="../js/app.js"></script>
+        <script src="js/app.js"></script>
 
     </body>
 </html>
